8b457bbbec19c1cbae8720e6f837c25e5e0f3f5a
Dhatchayani <dhatchu1810@gmail.com>
2026-01-07 18:04:11 +0530

    initial commit

--- app/src/main/java/com/simats/rentohub/RetrofitClient.kt
+++ app/src/main/java/com/simats/rentohub/RetrofitClient.kt
@@ -9,9 +9,9 @@ import java.util.concurrent.TimeUnit
 object RetrofitClient {
 
     // Emulator
-    const val BASE_URL = "http://192.168.137.1/rentohub/"
+//    const val BASE_URL = "http://172.23.52.0/rentohub/"
     // Physical device (same Wi-Fi)
-//     const val BASE_URL = "https://5gglqqk6-80.inc1.devtunnels.ms/rentohub/"
+     const val BASE_URL = "https://5gglqqk6-80.inc1.devtunnels.ms/rentohub/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
